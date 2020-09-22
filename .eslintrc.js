7ef1eee41cbf3ff3e00b251f434abe89c9e7935c
Paweł Stefański <pawelstefanski@MacBook-Pro-Pawe.local>
2020-09-22 23:16:08 +0200

    init

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'import/prefer-default-export': 0,
+    'react/jsx-filename-extension': [1,
+      {
+        extensions: [
+          '.tsx',
+        ],
+      },
+    ],
+  },
+};
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const c = 'string';
+
+console.log(c);
+
+document.body.append('OK');
+
+export function x() {
+  return <></>;
+}
